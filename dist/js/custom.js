68cf4ff7ea02d72ccb5b08b10e6cd1c31d0090e1
Sukhjinder <freezingfboxer@gmail.com>
2016-06-28 17:59:57 +0530

    today change done

--- dist/js/custom.js
+++ dist/js/custom.js
@@ -1,10 +1,11 @@
 
 
 		$(document).ready(function(){
-			$('.bxslider').bxSlider();
+			//$('.bxslider').bxSlider();
 			$('.slidtest').bxSlider({
 				auto:true
 			});
+
 			$(".navbar span").click(function(){
 				$(".main-ul").stop(true,false).slideToggle();
 			});
